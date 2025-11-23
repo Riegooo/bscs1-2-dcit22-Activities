f47df31a564ff7e0f1a412488966018471d9682a
Riegooo <christiandanielcagas0@gmail.com>
2025-11-23 19:42:10 +0800

    Initial

--- DeliveryFee_Calculator.py
+++ DeliveryFee_Calculator.py
@@ -1,7 +1,7 @@
 
-distance = float(input("Enter distance in km: "))
-is_member = input("Is the customer a member? (Yes | No): ").capitalize()
-order_amount = float(input("Enter your order amount: "))
+distance = float(input("Enter distance in km : "))
+is_member = input("Is the customer a member? (Yes | No) : ").capitalize()
+order_amount = float(input("Enter your order amount : "))
 
 if is_member == "Yes":
     is_member = True
@@ -34,8 +34,7 @@ if order_amount >= 1000:
 
 else:
     if is_member == True:
-        discount_fee = DeliveryDiscount_Fee - (DeliveryDiscount_Fee *
-        0.20)
+        discount_fee = DeliveryDiscount_Fee - (DeliveryDiscount_Fee * 0.20)
         total = order_amount + discount_fee
 
         print("\n==============================")
@@ -43,7 +42,7 @@ else:
         print("Original Delivery Fee:", DeliveryDiscount_Fee)
         print("Discounted Delivery Fee:", discount_fee)
         print("------------------------------")
-        print("Delivery fee has been discounted because the customer is aregistered member.")
+        print("Delivery fee has been discounted because the customer is a registered member.")
         print("==============================")
         print("Total Amount to Pay:", total)
         print("==============================")
@@ -58,3 +57,4 @@ else:
         print("==============================")
         print(f"Total Amount to Pay: {total}")
         print("==============================")
+        
